8729e19da45cb08cd2f8cc3b5a5f892f6608a3b3
yunpeng.song <yunpeng.song@thermofisher.com>
2018-06-23 10:07:50 +0800

    fix transaction for sqlserviceImpl

--- src/main/java/com/ddm/service/SqlServiceImpl.java
+++ src/main/java/com/ddm/service/SqlServiceImpl.java
@@ -4,6 +4,8 @@ import com.ddm.dao.CommonSqlMapper;
 import com.ddm.dao.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,7 @@ public class SqlServiceImpl implements SqlService {
     @Autowired
     private CommonSqlMapper commonSqlMapper;
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public List<Map<String, ?>> runSql(String sql) {
         return commonSqlMapper.select(sql);
